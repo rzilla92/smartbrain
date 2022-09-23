7c57a44b1ed599d35bca8b9848fd122fac75fb89
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-09-23 17:23:50 +0700

    added Particles bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Navigation, ImageLinkForm, FaceRecog, Rank } from './Components'
 import './App.css'
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg className="particles" color="#1DB4E0" type="cobweb" bg={true}/>
       <Navigation />
       <Rank />
       <ImageLinkForm />
