501dd5ccdb7b287c2e15f75865784421c0d7fb26
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-09-26 14:58:46 +0700

    broken api link ftm

--- src/Components/FaceRecog/FaceRecog.jsx
+++ src/Components/FaceRecog/FaceRecog.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './FaceRecog.css'
 
-function FaceRecog() {
+const FaceRecog = () => {
   return (
-    <div>FaceRecog</div>
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img id='inputimage' src={imageUrl} alt="imagedetect" width='500px' height='auto' />
+        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+      </div>
+    </div>
   )
 }
 
